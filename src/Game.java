a109bf2ec12a85dc1c3a1ef0538daa7b72dd7cbb
bribrah <cheesemas46@gmail.com>
2018-11-18 00:05:00 -0800

    fixed splash image

--- src/Game.java
+++ src/Game.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -64,7 +63,7 @@ class Game extends JPanel implements KeyListener, ActionListener{
         doubleBuffer = createImage(getWidth(),getHeight() - 50);
         doubleBufferGraphics = doubleBuffer.getGraphics();
         try {
-            splashScreen = ImageIO.read(new File("src/resources/splash image.png"));
+            splashScreen = ImageIO.read(new File("splash image.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
